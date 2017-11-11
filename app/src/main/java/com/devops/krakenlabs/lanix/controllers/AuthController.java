cbd855e5f45958dd136631182a8ee8ecd306a511
Alan Giovani Cruz Mendez <cruzmendezalan@gmail.com>
2017-11-11 12:27:49 -0600

    Commit inicial, esqueletos

---
+++ app/src/main/java/com/devops/krakenlabs/lanix/controllers/AuthController.java
@@ -0,0 +1,9 @@
+package com.devops.krakenlabs.lanix.controllers;
+
+/**
+ * Created by Alan Giovani Cruz Méndez on 11/11/17 12:26.
+ * cruzmendezalan@gmail.com
+ */
+
+public class AuthController {
+}
---
+++ app/src/main/java/com/devops/krakenlabs/lanix/models/session/Session.java
@@ -0,0 +1,9 @@
+package com.devops.krakenlabs.lanix.models.session;
+
+/**
+ * Created by Alan Giovani Cruz Méndez on 11/11/17 12:22.
+ * cruzmendezalan@gmail.com
+ */
+
+public class Session {
+}
