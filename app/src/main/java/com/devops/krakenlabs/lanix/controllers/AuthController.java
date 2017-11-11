a753fe82cbcae1345b18b2d2d267b613c6c621d1
Alan Giovani Cruz Mendez <cruzmendezalan@gmail.com>
2017-11-11 14:08:18 -0600

    Se obtienen urls del archivo de propiedades

--- app/src/main/java/com/devops/krakenlabs/lanix/controllers/AuthController.java
+++ app/src/main/java/com/devops/krakenlabs/lanix/controllers/AuthController.java
@@ -1,9 +1,23 @@
 package com.devops.krakenlabs.lanix.controllers;
 
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.devops.krakenlabs.lanix.base.LanixApplication;
+
 /**
  * Created by Alan Giovani Cruz Méndez on 11/11/17 12:26.
  * cruzmendezalan@gmail.com
  */
 
 public class AuthController {
+    private static final String TAG = AuthController.class.getSimpleName();
+    private Context context;
+    // Instantiate the RequestQueue.
+//    RequestQueue queue = Volley.newRequestQueue();
+
+    public AuthController(Context context) {
+        this.context = context;
+    }
 }
