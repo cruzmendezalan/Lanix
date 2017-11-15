b97b054ac2d3853b8560bb658d8c02d911021393
Alan Giovani Cruz Mendez <cruzmendezalan@gmail.com>
2017-11-15 00:50:01 -0600

    Se integra controlador de GPS

--- app/src/main/java/com/devops/krakenlabs/lanix/controllers/GPSController.java
+++ app/src/main/java/com/devops/krakenlabs/lanix/controllers/GPSController.java
@@ -1,65 +1,179 @@
 package com.devops.krakenlabs.lanix.controllers;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Service;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.util.Log;
-
-import com.devops.krakenlabs.lanix.HomeActivity;
+import android.widget.Toast;
 
 /**
- * Created by Alan Giovani Cruz Méndez on 12/11/17 14:58.
+ * Created by Alan Giovani Cruz Méndez on 12/11/17 18:37.
  * cruzmendezalan@gmail.com
  */
 
-public class GPSController implements ActivityCompat.OnRequestPermissionsResultCallback{
+public class GPSController extends Service implements LocationListener {
     public static String TAG = GPSController.class.getSimpleName();
-    private Context context;
-    private HomeActivity homeActivity;
-    private static GPSController gpsController;
-    private Boolean mLocationPermissionGranted;
-    /**
-     * Id to identify a gps permission request.
-     */
-    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private final Context mContext;
+    boolean checkGPS = false;
+    boolean checkNetwork = false;
+    boolean canGetLocation = false;
+    Location loc;
+    double latitude;
+    double longitude;
 
-    //Singleton
-    public static synchronized GPSController getInstance(Context context){
-        if (gpsController == null){
-            gpsController = new GPSController(context);
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
+    protected LocationManager locationManager;
+
+    public GPSController(Context mContext) {
+        this.mContext = mContext;
+        getLocation();
+    }
+
+    private Location getLocation() {
+
+        try {
+            locationManager = (LocationManager) mContext
+                    .getSystemService(LOCATION_SERVICE);
+
+            // get GPS status
+            checkGPS = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // get network provider status
+            checkNetwork = locationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!checkGPS && !checkNetwork) {
+                Toast.makeText(mContext, "No hay provedor de servicios disponible", Toast.LENGTH_SHORT).show();
+            } else {
+                this.canGetLocation = true;
+
+                // if GPS Enabled get lat/long using GPS Services
+                if (checkGPS) {
+
+                    if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Implement retrocompatibility
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        Log.e(TAG, "getLocation: NO PERMISOS" );
+                    }
+                    locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    if (locationManager != null) {
+                        loc = locationManager
+                                .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        if (loc != null) {
+                            latitude = loc.getLatitude();
+                            longitude = loc.getLongitude();
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return gpsController;
+
+        return loc;
     }
 
-    public GPSController(Context context) {
-        this.context = context;
+    public double getLongitude() {
+        if (loc != null) {
+            longitude = loc.getLongitude();
+        }
+        return longitude;
     }
 
-    public void setHomeActivity(HomeActivity homeActivity) {
-        this.homeActivity = homeActivity;
-        getLocationPermission();
+    public double getLatitude() {
+        if (loc != null) {
+            latitude = loc.getLatitude();
+        }
+        return latitude;
     }
 
-    private void getLocationPermission() {
-    /*
-     * Request location permission, so that we can get the location of the
-     * device. The result of the permission request is handled by a callback,
-     * onRequestPermissionsResult.
-     */
-        if (ContextCompat.checkSelfPermission(context.getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            mLocationPermissionGranted = true;
-        } else {
-            ActivityCompat.requestPermissions(homeActivity,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+    public boolean canGetLocation() {
+        return this.canGetLocation;
+    }
+
+    public void showSettingsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext);
+        alertDialog.setTitle("GPS apagado!");
+        alertDialog.setMessage("¿Deseas prender tú GPS?");
+        alertDialog.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                mContext.startActivity(intent);
+            }
+        });
+
+
+        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+
+        alertDialog.show();
+    }
+
+
+    public void stopListener() {
+        if (locationManager != null) {
+
+            if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationManager.removeUpdates(GPSController.this);
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        Log.d(TAG, "onRequestPermissionsResult() called with: requestCode = [" + requestCode + "], permissions = [" + permissions + "], grantResults = [" + grantResults + "]");
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
