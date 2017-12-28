73b5c065d4c77ef752ba5e3645d404aafb4a9c12
Alan Giovani Cruz Mendez <cruzmendezalan@gmail.com>
2017-12-28 14:37:53 -0600

    Se agrega modelo de request de catalogos

---
+++ app/src/main/java/com/devops/krakenlabs/lanix/models/catalogos/CatalogRequest.java
@@ -0,0 +1,30 @@
+package com.devops.krakenlabs.lanix.models.catalogos;
+
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
+/**
+ * Created by Alan Giovani Cruz Méndez on 28/12/17 14:08.
+ * cruzmendezalan@gmail.com
+ */
+
+public class CatalogRequest {
+    private static final String TAG = CatalogRequest.class.getSimpleName();
+
+    public JSONObject toJson(){
+        JSONObject jsonObject = null;
+        String jsonString = "";
+        try {
+            Gson gson  = new Gson();
+            jsonString = gson.toJson(this);
+            jsonObject = new JSONObject(jsonString);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        Log.e(TAG, "toJson: "+jsonString );
+        return jsonObject;
+    }
+}
