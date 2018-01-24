46dc11705e6b7d7e6069d1265f01c7e4bb721f2b
Alan Giovani Cruz Mendez <alan.cruz@walmart.com>
2018-01-24 00:02:52 -0600

    Se integran nuevas notificaciones

--- app/src/main/java/com/devops/krakenlabs/lanix/ventas/VentasThirdStepFragment.java
+++ app/src/main/java/com/devops/krakenlabs/lanix/ventas/VentasThirdStepFragment.java
@@ -1,9 +1,16 @@
 package com.devops.krakenlabs.lanix.ventas;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -21,11 +28,15 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.devops.krakenlabs.lanix.HomeActivity;
 import com.devops.krakenlabs.lanix.R;
 import com.devops.krakenlabs.lanix.base.LanixApplication;
+import com.devops.krakenlabs.lanix.models.venta.VentaResponse;
 import com.devops.krakenlabs.lanix.models.venta.VentasRequestt;
+import com.google.gson.Gson;
 import com.stepstone.stepper.Step;
 import com.stepstone.stepper.VerificationError;
 
 import org.json.JSONObject;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 /**
  * Created by Alan Giovani Cruz Méndez on 17/01/18 22:34.
@@ -37,6 +48,8 @@ public class VentasThirdStepFragment extends Fragment implements Step, Response.
     private View rootView;
     private ListView lvProductos;
     private Button btnConfirmarVenta;
+    public static final String channelId = "4565";
+    public static final CharSequence channelName = "LanixChannel";
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -65,7 +78,18 @@ public class VentasThirdStepFragment extends Fragment implements Step, Response.
                         public void onResponse(JSONObject response) {
                             Log.d(TAG, "onResponse() called with: response = [" + response + "]");
                             if (response != null){
-                                notificarVenta();
+                                Gson g = new Gson();
+                                try{
+                                    VentaResponse ve = g.fromJson(response.toString(),VentaResponse.class);
+                                    if (ve.getVentaId() > 0){
+                                        sendNotification("Venta registrada", "Se a registrado correctamente tu venta con el folio "+ve.getVentaId());
+                                    }else{
+                                        sendNotification("¡Ooops!", "Al parecer hemos tenido un problema al registrar tu venta");
+                                    }
+                                }catch (Exception e){
+                                    e.printStackTrace();
+                                    sendNotification("¡Ooops!", "Al parecer hemos tenido un problema al registrar tu venta");
+                                }
                             }
                         }
                     },this);
@@ -82,8 +106,53 @@ public class VentasThirdStepFragment extends Fragment implements Step, Response.
         snackBarView.setBackgroundColor(getResources().getColor(R.color.white));
         sn.show();
         Log.e(TAG, "notificarVenta: validar venta" );
+
+    }
+
+    private void sendNotification(String title, String text) {
+        makeAChannel();
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            Notification notification  = new Notification.Builder(getContext())
+                    .setCategory(Notification.CATEGORY_MESSAGE)
+                    .setContentTitle(title)
+                    .setContentText(text)
+                    .setAutoCancel(true)
+                    .setChannelId(channelId)
+                    .setSmallIcon(R.drawable.ic_create_24dp)
+                    .setVisibility(Notification.VISIBILITY_PUBLIC).build();
+            NotificationManager notificationManager =
+                    (NotificationManager) getActivity().getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.notify(1212212, notification );
+        }else{
+            Notification notification  = new Notification.Builder(getContext())
+                    .setCategory(Notification.CATEGORY_MESSAGE)
+                    .setContentTitle(title)
+                    .setContentText(text)
+                    .setAutoCancel(true)
+                    .setSmallIcon(R.drawable.ic_create_24dp)
+                    .setVisibility(Notification.VISIBILITY_PUBLIC).build();
+            NotificationManager notificationManager =
+                    (NotificationManager) getActivity().getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.notify(1212212, notification );
+        }
         Intent home = new Intent(getContext(), HomeActivity.class);
         startActivity(home);
+    }
+
+    private void makeAChannel(){
+        NotificationManager notificationManager =
+                (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+        int importance = NotificationManager.IMPORTANCE_LOW;
+        NotificationChannel notificationChannel = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            notificationChannel = new NotificationChannel(channelId, channelName, importance);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
     }
 
     private void initListView() {
